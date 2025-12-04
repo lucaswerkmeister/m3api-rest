097887dbbe5ce26824703367b1f39956132d2981
Lucas Werkmeister <mail@lucaswerkmeister.de>
2025-12-04 01:33:58 +0100

    Add `path` template literal tag function
    This can be used to correctly encode the path for a request. (The usage
    example should probably be changed to involve a request function, rather
    than console.log(), once request functions exist.)
    
    I’m not yet completely sure if the escaping is correct, because upon
    trying to test it I quickly discovered a bug in the REST API where it
    doesn’t correctly redirect. Once T411738 has been fixed (and the fix has
    been deployed in Wikimedia production), I should try out what the
    actually expected URL for "?! (album)" is, and potentially adjust the
    test case.

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 /**
- * @param {*} v
- * @return {*}
+ * Encode a path for a REST API endpoint.
+ *
+ * This function should be used as a tag for a tagged template literal (template string).
+ * Usage example:
+ * ```
+ * const title = 'AC/DC';
+ * console.log( path`/page/${ title }` );
+ * // logs /page/AC%2FDC
+ * ```
+ *
+ * (Calling this function like a regular function is not very useful,
+ * so you can ignore the parameters documented below.)
+ *
+ * @param {string[]} strings
+ * @param {Array} values
+ * @return {string}
  */
-export function todo( v ) {
-	return v;
+export function path( strings, ...values ) {
+	return String.raw( { raw: strings }, ...values.map( encodeURIComponent ) );
 }
--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -1,17 +1,45 @@
 /* eslint-env mocha */
 
 import {
-	todo,
+	path,
 } from '../../index.js';
 import { expect, use } from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 use( chaiAsPromised );
 
-describe( 'todo', () => {
+describe( 'path', () => {
 
-	it( 'returns its argument', () => {
-		const v = 123;
-		expect( todo( v ) ).to.equal( v );
+	it( 'URI-encodes one parameter', () => {
+		const title = 'AC/DC';
+		const actual = path`/v1/page/${ title }/html`;
+		expect( actual ).to.equal( '/v1/page/AC%2FDC/html' );
+	} );
+
+	it( 'URI-encodes two parameters', () => {
+		const title = '?! (album)';
+		const type = 'anonymous/temporary'; // fake value
+		const actual = path`/v1/page/${ title }/history/counts/${ type }`;
+		// TODO verify that the expected URL is correct
+		// (unencoded parentheses and exclamation mark? space becomes %20 instead of underscore?)
+		// once T411738 is fixed
+		expect( actual ).to.equal( '/v1/page/%3F!%20(album)/history/counts/anonymous%2Ftemporary' );
+	} );
+
+	it( 'URI-encodes three parameters', () => {
+		const id = 'I/D'; // fake value
+		const wiki = 'en/wiki'; // fake value
+		const revid = 'rev/id'; // fake value
+		const actual = path`/campaignevents/v0/event_registration/${ id }/edits/${ wiki }/${ revid }`;
+		expect( actual ).to.equal( '/campaignevents/v0/event_registration/I%2FD/edits/en%2Fwiki/rev%2Fid' );
+	} );
+
+	it( 'URI-encodes four parameters', () => {
+		const skin = 'vector/2022'; // fake value
+		const editor = 'visual/editor'; // fake value
+		const tasktypeid = 'copy/edit'; // fake value
+		const uselang = 'en/gb'; // fake value
+		const actual = path`/growthexperiments/v0/quickstarttips/${ skin }/${ editor }/${ tasktypeid }/${ uselang }`;
+		expect( actual ).to.equal( '/growthexperiments/v0/quickstarttips/vector%2F2022/visual%2Feditor/copy%2Fedit/en%2Fgb' );
 	} );
 
 } );
